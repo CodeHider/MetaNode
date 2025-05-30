0870e9f4d029f7837b73dc62517b177c38a1340e
coderHider <coderjoker2021@163.com>
2025-05-30 23:19:51 +0800

    metaNode golang学习,字符串:有效的括号,最长字符串前缀

--- base1_golang/chapter3/字符串.go
+++ base1_golang/chapter3/字符串.go
@@ -37,3 +37,42 @@ func IsValid(s string) bool {
 	}
 	return false
 }
+
+/*
+*
+最长公共前缀
+
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+*/
+func LongestCommonPrefix(strs []string) string {
+	//k:表示字符的下标位置,出现了几次. index_c
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		prefix = getSingleLongest(prefix, strs[i])
+		if len(prefix) == 0 {
+			break
+		}
+	}
+	return prefix
+}
+
+func getSingleLongest(prefix string, str2 string) string {
+	length := min(len(prefix), len(str2))
+	index := 0
+	for index < length && prefix[index] == str2[index] {
+		index++
+	}
+	//在之前已经截取的字符串中的前index字符
+	return prefix[:index]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
